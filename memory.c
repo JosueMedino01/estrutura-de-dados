fede7f86764bcf9ac7bac5f7c8b0022fd9d87c50
Josué Medino <josuemedino5@gmail.com>
2023-11-28 22:09:36 -0300

    feat: Memory introdution

--- memory.c
+++ memory.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (){
-    //A memória 
+    //O computador separa um espaço de 4 bytes para 'value'
     int value;
 
-    int *andress; 
+   /*
+        A memória é preenchida com valoes binários 
+        'value = 2'  equivale a :
+        
+        00000000  
+        00000000 
+        00000000 
+        00000010
 
+        onde cada quebra de linha representa um byte com 8 bits 
+   */
+    value = 2; 
+
+    //O computador reserva 8 bytes para o ponteiro
+    int *andress;
+
+    //'andress' recebe 0x7ffd27e93e7c como posição base
+    andress = &value;
+
+    printf("%d\n", *andress); // Saida: 2 (conteudo)_
+    printf("%p\n", andress); // Saida: 0x7ffd27e93e7c (endereco)
+
+    return 0;
 }
